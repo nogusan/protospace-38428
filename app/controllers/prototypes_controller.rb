1eb8aee904a5532d3e60fcd7e4306d0c115a4afd
nogusan <standing.nogusan@gmail.com>
2022-12-16 20:30:17 +0900

    2_2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -34,7 +34,8 @@ class PrototypesController < ApplicationController
     if prototype.update(prototype_params)
       redirect_to prototype_path(prototype.id)
     else
-      render edit_prototype_path(prototype.id)
+      @prototype = Prototype.new(prototype_params)
+      render :edit
     end
   end
 
